0b6053d59912c3dc6ec2bbd244c1a56b2ffc11be
mateo florez jaramillo <mateo.florez.9594@eam.edu.co>
2026-04-10 19:09:35 -0500

    feat: integrate Twilio for WhatsApp message sending

--- src/services/whatsapp.service.js
+++ src/services/whatsapp.service.js
@@ -1,7 +1,25 @@
 // services/whatsapp.service.js
+import axios from "axios";
+
+const TWILIO_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH = process.env.TWILIO_AUTH_TOKEN;
+const TWILIO_NUMBER = process.env.TWILIO_PHONE_NUMBER;
 
 export const sendWhatsAppMessage = async (to, message) => {
-  console.log(`📤 Enviando a ${to}: ${message}`);
+  try {
+    const url = `https://api.twilio.com/2010-04-01/Accounts/${TWILIO_SID}/Messages.json`;
 
-  // Aquí luego conectas Twilio o Meta API
+    const params = new URLSearchParams();
+    params.append("From", `whatsapp:${TWILIO_NUMBER}`);
+    params.append("To", `whatsapp:${to}`);
+    params.append("Body", message);
+
+    await axios.post(url, params, {
+      auth: { username: TWILIO_SID, password: TWILIO_AUTH },
+    });
+
+      console.log(`📤 Mensaje enviado a ${to}`);
+    } catch (error) {
+      console.error("Error enviando mensaje:", error.response?.data || error.message);
+    }
 };
